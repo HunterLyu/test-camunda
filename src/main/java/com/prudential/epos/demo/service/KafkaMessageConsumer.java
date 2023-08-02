479470c5839eb497fc48b5cd3bc40599f4ce3212
HunterLyu <shiyu.lv@prudential.com.sg>
2023-08-02 10:01:57 +0800

    update files.

---
+++ src/main/java/com/prudential/epos/demo/service/KafkaMessageConsumer.java
@@ -0,0 +1,19 @@
+package com.prudential.epos.demo.service;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaMessageConsumer {
+    protected Logger log = LoggerFactory.getLogger(getClass());
+
+
+    @KafkaListener(topics = {"task1FinishTopic", "task1SendTopic", "task2FinishTopic", "task2SendTopic",
+            "task3SendTopic", "task3FinishTopic"})
+    public void onMessage1(ConsumerRecord<?, ?> record) {
+        log.info("consume message of:：" + record.topic() + "-" + record.partition() + "-" + record.value());
+    }
+}
